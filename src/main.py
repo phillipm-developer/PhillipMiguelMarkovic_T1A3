375181498d2fe8d4fb45d51ea368cc5ed82f2812
phillipm-developer <14167@coderacademy.edu.au>
2023-05-09 01:57:40 +1000

    Reading and writing csv and json files independantly

--- src/main.py
+++ src/main.py
@@ -13,18 +13,19 @@ def isfloat(num):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-e", "--equation", help="Expression to evaluate")
-parser.add_argument("-v", "--value", help="Numerical value to subtitute into equation")
-parser.add_argument("-vs", "--values", help="Solve for these values")
-parser.add_argument("-i", "--inputfile", help="Input file to draw values from")
-parser.add_argument("-o", "--outputfile", help="Output file for evaluated answers")
+# parser.add_argument("-e", "--equation", help="Expression to evaluate")
+# parser.add_argument("-v", "--value", help="Numerical value to subtitute into equation")
+# parser.add_argument("-vs", "--values", help="Solve for these values")
+
+parser.add_argument("-i", "--input_file", type=str, help="CSV or JSON input file containing equation & values")
+parser.add_argument("-o", "--output_file", type=str, help="CSV or JSON output file for evaluated answers")
+
 
 args = parser.parse_args()
-
 # print(args)
 
 args_dict = vars(args)
-# print(args_dict)
+print(args_dict)
 
 exp = ""
 equation = []
@@ -33,6 +34,9 @@ def interactive_mode():
     while (True):
         try:
             exp = input("Please enter an expression> ")
+
+            if exp.lower() == "quit" or exp.lower() == "exit":
+                break
 
             equation = expression.Expression(exp)
 
@@ -66,11 +70,11 @@ def interactive_mode():
         except expression.syntax_exception.SyntaxException as err:
             print(err.get_message())
 
-def process_csv_file():
+def process_csv_file(input_csv_file):
     output_list = []
 
     # Open the input csv file and read each row from it
-    with open('input.csv') as f:
+    with open(input_csv_file) as f:
         reader = csv.DictReader(f)
         for row in reader:
 
@@ -95,27 +99,41 @@ def process_csv_file():
             calculation_dict['substitutions'] = substitutions
             calculation_dict['values_obtained'] = True
 
-            # Evaluate the euqation using the data in the calculation dictionary and set the result
+            # Evaluate the equation using the data in the calculation dictionary and set the result
             calculation_dict = equation.evaluate_calc_dict(calculation_dict)
-            print(calculation_dict)
+            # print(calculation_dict)
 
-            row['result'] = calculation_dict['result']
-            print(row)
-            output_list.append(row)
+            # row['result'] = calculation_dict['result']
+            # print(row)
+
+            output_list.append(calculation_dict)
 
     f.close()  # Close the file handle for the input file
+    return output_list
 
     # Open the output file and write the header and the results
-    with open('output.csv', 'w') as f:
-        writer = csv.DictWriter(f, output_list[0].keys())
-        writer.writeheader()
-        writer.writerows(output_list)
+    # with open('output.csv', 'w') as f:
+    #     writer = csv.DictWriter(f, output_list[0].keys())
+    #     writer.writeheader()
+    #     writer.writerows(output_list)
 
-    f.close()  # Close the file handle for the output file
+    # f.close()  # Close the file handle for the output file
 
+# Open the output file and write the header and the results
+def write_csv_file(output_list, output_csv_file):
+    write_list = []
+    for dict in output_list:
+        row = f"{output_list},"
 
-def process_json_file():
-    with open('input.json', 'r') as f:
+    # with open(output_csv_file, 'w') as f:
+    #     writer = csv.DictWriter(f, output_list[0].keys())
+    #     writer.writeheader()
+    #     writer.writerows(output_list)
+
+    # f.close()  # Close the file handle for the output file
+
+def process_json_file(input_json_file):
+    with open(input_json_file, 'r') as f:
         data = json.load(f)
 
     for calculation_dict in data:
@@ -123,15 +141,33 @@ def process_json_file():
         calculation_dict = equation.evaluate_calc_dict(calculation_dict)
         print(calculation_dict)
 
+    f.close()
+    return data
 
-    with open('output.json', 'w') as f:
+    # with open('output.json', 'w') as f:
+    #     json.dump(data, f)
+
+
+def write_json_file(data, output_json_file):
+    with open(output_json_file, 'w') as f:
         json.dump(data, f)
 
-# if args_dict['inputfile'] != 'None':
-#     input_file = args_dict['inputfile']
 
-# process_csv_file()
+input_file = args_dict['input_file']
+output_file = args_dict['output_file']
 
-process_json_file()
+output_list = []
+
+if input_file != None:  # None is a python keyword meaning null value
+    if '.json' in input_file:
+        output_list = process_json_file(input_file)
+    elif '.csv' in input_file:
+        output_list = process_csv_file(input_file)    
+
+if output_file != None:
+    if '.json' in output_file:
+        write_json_file(output_list, output_file)
+    elif '.csv' in output_file:
+        write_csv_file(output_list, output_file)
 
 # interactive_mode()
